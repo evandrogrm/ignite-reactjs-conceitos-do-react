cf28971c8b46daebf1568be8f575a8d0945517d2
Evandro Gaspar Ribeiro <evandrogrm@hotmail.com>
2021-07-06 09:07:13 -0300

    resolved challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return;
+    }
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksToogled = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+    setTasks(tasksToogled);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksWithIdRemoved = tasks.filter(task => task.id !== id);
+    setTasks(tasksWithIdRemoved);
   }
 
   return (
